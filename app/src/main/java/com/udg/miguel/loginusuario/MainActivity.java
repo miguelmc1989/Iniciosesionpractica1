6613a4543f8a3acc8ed3f9b606a653d1e948c242
miguelmc1989 <miguell_moreno@hotmail.com>
2015-09-22 19:09:59 -0500

    remplazo

--- app/src/main/java/com/udg/miguel/loginusuario/MainActivity.java
+++ app/src/main/java/com/udg/miguel/loginusuario/MainActivity.java
@@ -1,9 +1,14 @@
 package com.udg.miguel.loginusuario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button boton = (Button) findViewById(R.id.button);
+        boton.setOnClickListener(new View.OnClickListener(){
+            @Override
+
+           public void onClick(View view) {
+                String usuario = ((EditText)findViewById(R.id.txtusuario)).getText().toString();
+                String password = ((EditText)findViewById(R.id.txtcontraseña)).getText().toString();
+                if (usuario.equals("root" )&& password.equals("admin"))
+                {
+                    Intent nuevoform = new Intent(MainActivity.this,Ventana.class);
+                    startActivity(nuevoform);
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Usuario o Contraseña incorrectos",Toast.LENGTH_SHORT).show();
+                }
+        }
+
+        });
     }
 
     @Override
